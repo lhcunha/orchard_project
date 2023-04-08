f1cbe8e2016e685592f5094a850cfbcbea4d2f9a
lhcunha <lhcunha1@gmail.com>
2023-04-08 22:10:16 +0000

    db connection tests

--- testes.py
+++ testes.py
@@ -1,20 +1,17 @@
 import mysql.connector
-import time
 
 # Configurações de conexão com a base de dados
-host = 'mysql'
+host = 'localhost'
 user = 'orchard'
 password = 'orchardpassword'
 database = 'orchard'
-port = '3306'
-
+port = 3306
 
 try:
-    #tenta estabelecer a conexão
+    # Cria a conexão com a base de dados
     cnx = mysql.connector.connect(user=user, password=password, host=host, port=port, database=database)
     print("Conexão com o MySQL foi realizada com sucesso!")
     cnx.close()
 except mysql.connector.Error as err:
-    # se houver um erro, espera 1 segundo e tenta novamente
     print(f"Erro ao conectar com o MySQL: {err}")
-    time.sleep(1)
+
