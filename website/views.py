67b864bf3f8ef28e2a3246456f0d37eb8b7d920f
lhcunha <lhcunha1@gmail.com>
2023-05-04 02:57:09 +0000

    website initial version and set flask debug mode on

--- website/views.py
+++ website/views.py
@@ -3,7 +3,6 @@ import mysql.connector
 import os
 
 app = Flask(__name__)
-
 mysql_host = os.getenv("MYSQL_HOST", "mysql")
 mysql_port = os.getenv("MYSQL_PORT", 3306)
 mysql_user = os.getenv("MYSQL_USER", "orchard")
@@ -12,10 +11,10 @@ mysql_database = os.getenv("MYSQL_DATABASE", "orchard")
 
 # Configure database connection
 db = mysql.connector.connect(
-  host="mysql",
-  user="orchard",
-  password="orchardpassword",
-  database="orchard"
+  host = mysql_host,
+  user = mysql_user,
+  password = mysql_password,
+  database = mysql_database
 )
 
 # Define route to display companies grouped by industrial segment
@@ -23,7 +22,8 @@ db = mysql.connector.connect(
 def companies_by_segment():
     # Query database to get companies grouped by segment
     cursor = db.cursor()
-    query = "SELECT SETOR_ATIV, GROUP_CONCAT(name SEPARATOR ', ') as companies FROM CIAS_ABERTAS GROUP BY SETOR_ATIV"
+    query = "SELECT SETOR_ATIV, GROUP_CONCAT(DENOM_COMERC SEPARATOR ', ') as companies FROM CIAS_ABERTAS \
+            WHERE SIT = 'ATIVO' AND TP_MERC = 'BOLSA' GROUP BY SETOR_ATIV"
     cursor.execute(query)
     results = cursor.fetchall()
     
@@ -31,4 +31,4 @@ def companies_by_segment():
     return render_template('companies.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
